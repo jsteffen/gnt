17c37f68ed70897ffd7d4727e150459331fe622f
Günter Neumann <neumann@dfki.de>
2016-05-12 14:22:05 +0200

    updates

--- src/main/java/caller/TrainTagger.java
+++ src/main/java/caller/TrainTagger.java
@@ -12,11 +12,11 @@ public class TrainTagger {
 
 	public static void trainer(String dataConfigFileName, String corpusConfigFileName) throws IOException{
 		ModelInfo modelInfo = new ModelInfo();
-
 		GNTdataProperties dataProps = new GNTdataProperties(dataConfigFileName);
 		dataProps.copyConfigFile(dataConfigFileName);
 		GNTcorpusProperties corpusProps = new GNTcorpusProperties(corpusConfigFileName);
 		GNTrainer gnTrainer = new GNTrainer(modelInfo, dataProps, corpusProps);
+		
 		gnTrainer.gntTrainingWithDimensionFromConllFile(
 				corpusProps.getTrainingFile(), corpusProps.getClusterIdNameFile(), GlobalParams.dim, GlobalParams.numberOfSentences);
 	}
