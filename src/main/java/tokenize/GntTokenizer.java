a4c3ed334e98897536bde800fa0aa7a394f02438
Günter Neumann <neumann@dfki.de>
2016-06-10 18:42:14 +0200

    solved bug in GntTokenizer

--- src/main/java/tokenize/GntTokenizer.java
+++ src/main/java/tokenize/GntTokenizer.java
@@ -1,12 +1,26 @@
 package tokenize;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class GntTokenizer {
 
 	public static String[] splitTokenizer(String string){
-		System.out.println(">>>"+string+"<<<");
-		
+		//System.out.println(">>>"+string+"<<<");
+
 		String delims = "[ |\\,|\\:|\\.|\\\"|\\(|\\)|\\!|\\?]+";
-		return string.split(delims);
+		delims = "[\\W]+";
+
+		//TODO: may have empty tokens
+
+		String[] tokens = string.split(delims);
+
+		final List<String> list =  new ArrayList<String>();
+		Collections.addAll(list, tokens); 
+		list.remove("");
+		tokens = list.toArray(new String[list.size()]);
+		return tokens;
 	}
 
 }
