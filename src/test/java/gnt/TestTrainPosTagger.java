c58f7c8e38275a964c21acc02edbd98e9d000ee9
Günter Neumann <neumann@dfki.de>
2016-08-04 13:31:25 +0200

    add

--- src/test/java/gnt/TestTrainPosTagger.java
+++ src/test/java/gnt/TestTrainPosTagger.java
@@ -8,18 +8,18 @@ public class TestTrainPosTagger {
 
 	public static void main(String[] args) throws IOException{
 		TrainTagger gntTrainer = new TrainTagger();
-		//				gntTrainer.trainer(
-		//						"src/main/resources/dataProps/EnNerTagger.xml", 
-		//						"src/main/resources/corpusProps/EnNerTagger.xml");
+		gntTrainer.trainer(
+				"src/main/resources/dataProps/EnNerTagger.xml", 
+				"src/main/resources/corpusProps/EnNerTagger.xml");
 		//				gntTrainer.trainer(
 		//						"src/main/resources/dataProps/DeNerKonvTagger.xml", 
 		//						"src/main/resources/corpusProps/DeNerKonvTagger.xml");
-		gntTrainer.trainer(
-				"src/main/resources/dataProps/DePosTagger.xml", 
-				"src/main/resources/corpusProps/DePosTagger.xml");
-//		gntTrainer.trainer(
-//				"src/main/resources/dataProps/EnPosTagger.xml", 
-//				"src/main/resources/corpusProps/EnPosTagger.xml");
+		//		gntTrainer.trainer(
+		//				"src/main/resources/dataProps/DePosTagger.xml", 
+		//				"src/main/resources/corpusProps/DePosTagger.xml");
+		//		gntTrainer.trainer(
+		//				"src/main/resources/dataProps/EnPosTagger.xml", 
+		//				"src/main/resources/corpusProps/EnPosTagger.xml");
 		//		gntTrainer.trainer(
 		//				"src/main/resources/dataProps/EnUniPosTagger.xml", 
 		//				"src/main/resources/corpusProps/EnUniPosTagger.xml");
