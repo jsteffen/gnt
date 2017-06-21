6f15da3839d8f2cae5927679cec02dff6074fdbb
Jörg Steffen <steffen@dfki.de>
2017-06-21 15:37:45 +0200

    Add unit tests.
    Please note: Unit tests require CoNLL resources that are **not** contained in this repository.

---
+++ src/test/java/de/dfki/mlt/gnt/TestGNT.java
@@ -0,0 +1,140 @@
+package de.dfki.mlt.gnt;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.dfki.mlt.gnt.caller.TrainTagger;
+import de.dfki.mlt.gnt.config.ConfigKeys;
+import de.dfki.mlt.gnt.config.GlobalConfig;
+import de.dfki.mlt.gnt.tagger.GNTagger;
+
+/**
+ *
+ *
+ * @author Jörg Steffen, DFKI
+ */
+public class TestGNT {
+
+  @Before
+  public void setUp() throws IOException {
+
+    Utils.deleteFolder(GlobalConfig.getModelBuildFolder());
+    Utils.deleteFolder(GlobalConfig.getPath(ConfigKeys.EVAL_FOLDER));
+    List<Path> filesToDelete = Utils.getAllFilesFromFolder(Paths.get("src/test/resources"), "*.{zip,txt}");
+    filesToDelete.addAll(Utils.getAllFilesFromFolder(Paths.get("src/test/resources/input"), "*.GNT"));
+    for (Path oneFileToDelete : filesToDelete) {
+      Files.delete(oneFileToDelete);
+    }
+  }
+
+
+  @Test
+  public void testTrainEvalTag()
+      throws IOException {
+
+    GlobalConfig.getInstance().setProperty(ConfigKeys.CREATE_LIBLINEAR_INPUT_FILE, false);
+    GlobalConfig.getInstance().setProperty(ConfigKeys.DEBUG, true);
+
+    testTrain();
+    testEval();
+    testTag();
+  }
+
+
+  private void testTrain()
+      throws IOException {
+
+    TrainTagger gntTrainer = new TrainTagger();
+    gntTrainer.trainer(
+        "src/test/resources/dataProps/EnPosTagger.xml",
+        "src/test/resources/corpusProps/EnPosTagger.xml");
+
+    String modelName = "model_ENPOS_2_0iw-1sent_FTTTF_MCSVM_CS";
+    assertThat(GlobalConfig.getPath(ConfigKeys.MODEL_OUTPUT_FOLDER).resolve(modelName + ".zip")).exists();
+
+    List<Path> modelFiles =
+        Utils.getAllFilesFromFolder(GlobalConfig.getModelBuildFolder(), "*");
+    List<Path> expectedModelFiles =
+        Utils.getAllFilesFromFolder(Paths.get("src/test/resources/expected/model"), "*");
+
+    assertThat(modelFiles).hasSameSizeAs(expectedModelFiles);
+    modelFiles.sort(null);
+    expectedModelFiles.sort(null);
+    for (int i = 0; i < modelFiles.size(); i++) {
+      assertThat(modelFiles.get(i).getFileName()).isEqualTo(expectedModelFiles.get(i).getFileName());
+      assertThat(modelFiles.get(i)).usingCharset(StandardCharsets.UTF_8)
+          .hasSameContentAs(expectedModelFiles.get(i), StandardCharsets.UTF_8);
+    }
+  }
+
+
+  private void testEval()
+      throws IOException {
+
+    GNTagger tagger = new GNTagger("src/test/resources/model_ENPOS_2_0iw-1sent_FTTTF_MCSVM_CS.zip");
+    tagger.eval("src/test/resources/corpusProps/EnPosTagger.xml");
+
+    List<Path> evalFiles =
+        Utils.getAllFilesFromFolder(GlobalConfig.getPath(ConfigKeys.EVAL_FOLDER), "*");
+    List<Path> expectedEvalFiles =
+        Utils.getAllFilesFromFolder(Paths.get("src/test/resources/expected/eval"), "*");
+    assertThat(evalFiles).hasSameSizeAs(expectedEvalFiles);
+    evalFiles.sort(null);
+    expectedEvalFiles.sort(null);
+    for (int i = 0; i < evalFiles.size(); i++) {
+      assertThat(evalFiles.get(i).getFileName()).isEqualTo(expectedEvalFiles.get(i).getFileName());
+      assertThat(evalFiles.get(i)).usingCharset(StandardCharsets.UTF_8)
+          .hasSameContentAs(expectedEvalFiles.get(i), StandardCharsets.UTF_8);
+    }
+    tagger.close();
+  }
+
+
+  private void testTag()
+      throws IOException {
+
+    GNTagger tagger = new GNTagger("src/test/resources/model_ENPOS_2_0iw-1sent_FTTTF_MCSVM_CS.zip");
+    tagger.tagFolder("src/test/resources/input", "UTF-8", "UTF-8");
+
+    List<Path> taggedFiles =
+        Utils.getAllFilesFromFolder(Paths.get("src/test/resources/input"), "*.GNT");
+    List<Path> expectedTaggedFiles =
+        Utils.getAllFilesFromFolder(Paths.get("src/test/resources/expected/tagged"), "*.GNT");
+    assertThat(taggedFiles).hasSameSizeAs(expectedTaggedFiles);
+    taggedFiles.sort(null);
+    expectedTaggedFiles.sort(null);
+    for (int i = 0; i < taggedFiles.size(); i++) {
+      assertThat(taggedFiles.get(i).getFileName()).isEqualTo(expectedTaggedFiles.get(i).getFileName());
+      assertThat(taggedFiles.get(i)).usingCharset(StandardCharsets.UTF_8)
+          .hasSameContentAs(expectedTaggedFiles.get(i), StandardCharsets.UTF_8);
+    }
+    tagger.close();
+  }
+
+
+  @Test
+  public void testTrainLiblinearInput() throws IOException {
+
+    GlobalConfig.getInstance().setProperty(ConfigKeys.CREATE_LIBLINEAR_INPUT_FILE, true);
+
+    TrainTagger gntTrainer = new TrainTagger();
+    gntTrainer.trainer(
+        "src/test/resources/dataProps/EnPosTagger.xml",
+        "src/test/resources/corpusProps/EnPosTagger.xml");
+    String modelName = "model_ENPOS_2_0iw-1sent_FTTTF_MCSVM_CS";
+    assertThat(GlobalConfig.getPath(ConfigKeys.MODEL_OUTPUT_FOLDER).resolve(modelName + ".zip")).exists();
+    String liblinearInputFileName = "liblinear_input_" + modelName + ".txt";
+    assertThat(GlobalConfig.getPath(ConfigKeys.MODEL_OUTPUT_FOLDER).resolve(liblinearInputFileName))
+        .usingCharset(StandardCharsets.UTF_8).hasSameContentAs(
+            Paths.get("src/test/resources/expected").resolve(liblinearInputFileName), StandardCharsets.UTF_8);
+  }
+}
