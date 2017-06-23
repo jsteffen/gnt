cbb200264abedeff42edfd428b63782bfdf92830
Jörg Steffen <steffen@dfki.de>
2017-06-23 12:45:07 +0200

    Add training method that allows to override archive name

--- src/main/java/de/dfki/mlt/gnt/caller/TrainTagger.java
+++ src/main/java/de/dfki/mlt/gnt/caller/TrainTagger.java
@@ -1,7 +1,14 @@
 package de.dfki.mlt.gnt.caller;
 
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
+import de.dfki.mlt.gnt.config.GlobalConfig;
 import de.dfki.mlt.gnt.corpus.GNTcorpusProperties;
 import de.dfki.mlt.gnt.data.GNTdataProperties;
 import de.dfki.mlt.gnt.trainer.GNTrainer;
@@ -13,13 +20,35 @@ import de.dfki.mlt.gnt.trainer.GNTrainer;
  */
 public class TrainTagger {
 
-  public void trainer(String dataConfigFileName, String corpusConfigFileName) throws IOException {
+  public void trainer(String modelConfigFileName, String corpusConfigFileName)
+      throws IOException {
 
-    GNTdataProperties dataProps = new GNTdataProperties(dataConfigFileName);
+    this.trainer(modelConfigFileName, corpusConfigFileName, null);
+  }
+
+
+  /**
+   * This one used for processing the universal dependency treebanks
+   * cf. com.gn.UDlanguageGNTmodelFactory.trainLanguage(String, String)
+   * @param modelConfigFileName
+   * @param corpusConfigFileName
+   * @param modelArchiveName
+   * @throws IOException
+   */
+  public void trainer(String modelConfigFileName, String corpusConfigFileName, String modelArchiveName)
+      throws IOException {
+
+    deleteFolder(GlobalConfig.getModelBuildFolder());
+
+    GNTdataProperties dataProps = new GNTdataProperties(modelConfigFileName);
     GNTcorpusProperties corpusProps = new GNTcorpusProperties(corpusConfigFileName);
     GNTrainer gnTrainer = new GNTrainer(dataProps, corpusProps);
 
-    dataProps.copyConfigFile(dataConfigFileName);
+    dataProps.copyConfigFile(modelConfigFileName);
+
+    if (modelArchiveName != null) {
+      gnTrainer.getArchivator().setArchiveName(modelArchiveName);
+    }
 
     gnTrainer.gntTrainingWithDimensionFromConllFile(
         corpusProps.getTrainingFile(), corpusProps.getClusterIdNameFile(),
@@ -28,32 +57,31 @@ public class TrainTagger {
   }
 
 
-  /**
-   * This one used for processing the universal dependency treebanks
-   * cf. com.gn.UDlanguageGNTmodelFactory.trainLanguage(String, String)
-   * @param dataConfigFileName
-   * @param corpusConfigFileName
-   * @param modelZipFileName
-   * @param archiveTxtName
-   * @throws IOException
-   */
-  public void trainer(String dataConfigFileName, String corpusConfigFileName,
-      String modelZipFileName, String archiveTxtName) throws IOException {
+  private static void deleteFolder(Path path)
+      throws IOException {
 
-    GNTdataProperties dataProps = new GNTdataProperties(dataConfigFileName);
-    GNTcorpusProperties corpusProps = new GNTcorpusProperties(corpusConfigFileName);
-    GNTrainer gnTrainer = new GNTrainer(dataProps, corpusProps);
+    try {
+      Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
 
-    dataProps.copyConfigFile(dataConfigFileName);
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+            throws IOException {
 
-    //GN: Major difference to above method
-    dataProps.getModelInfo().setModelName(archiveTxtName);
-    gnTrainer.getArchivator().setArchiveName(modelZipFileName);
+          Files.delete(file);
+          return FileVisitResult.CONTINUE;
+        }
 
 
-    gnTrainer.gntTrainingWithDimensionFromConllFile(
-        corpusProps.getTrainingFile(), corpusProps.getClusterIdNameFile(),
-        dataProps.getGlobalParams().getDim(),
-        dataProps.getGlobalParams().getNumberOfSentences());
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc)
+            throws IOException {
+
+          Files.delete(dir);
+          return FileVisitResult.CONTINUE;
+        }
+      });
+    } catch (NoSuchFileException e) {
+      // nothing to do, file already deleted
+    }
   }
 }
